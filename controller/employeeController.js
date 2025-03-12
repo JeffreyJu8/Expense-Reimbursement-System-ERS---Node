eb0c7a189f6641c4462394ebdee706a6293cce1d
jeffrey ju <jeffreyju1899@gmail.com>
2025-03-12 11:32:51 -0700

    middleware

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -3,17 +3,42 @@ const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
 const employeeService = require("../service/employeeService");
 
-router.post("/", async (req, res) => {
+router.post("/", validateRegisterMiddleware, async (req, res) => {
 
     const {username, password} = req.body;
     // const jsonData = req.body;
     //console.log("Adding Employee: ", jsonData);
 
-    const newEmployee = { employee_id: uuidv4(), username, password};
+    const newEmployee = { employee_id: uuidv4(), username, password, role};
 
     const data = await employeeService.registerEmployee(newEmployee);
 
     res.status(201).json({message: "Employee Created: ", Employee: data});
 });
+
+function validateRegisterMiddleware(req, res, next){
+    const jsonBody = req.body;
+
+    if(validateRegister(jsonBody)){
+        next();
+    }
+    else{
+        res.status(400).json({
+            message: "Invalid username or password!"
+        });
+    }
+}
+
+function validateRegister(data){
+    return (data.username && data.password && role);
+}
+
+function validateUsername(data){
+    const params = {
+        TableName: "Employees",
+
+    }
+}
+
 
 module.exports = router;
